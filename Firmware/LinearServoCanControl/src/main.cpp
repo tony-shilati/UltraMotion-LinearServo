d84c15b9ac5c41e0bd8a38d9f006ece5ae3a66f8
Rohan Kota <rohankota@rohans-mbp-9.ads.northwestern.edu>
2025-10-13 17:24:24 -0500

    minimal CAN example

--- Firmware/LinearServoCanControl/src/main.cpp
+++ Firmware/LinearServoCanControl/src/main.cpp
@@ -1,18 +1,30 @@
-#include <Arduino.h>
+#include <FlexCAN_T4.h>
 
-// put function declarations here:
-int myFunction(int, int);
+FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> can3;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(1000);
+
+  can3.begin();
+  can3.setBaudRate(1000000); // 1 Mbps
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  uint16_t number = 3000; // Position command
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  CAN_message_t msg;
+  msg.id = 0x3;
+  msg.len = 2; // Sending 2 bytes
+
+  msg.buf[0] = number & 0xFF;        // Low byte
+  msg.buf[1] = (number >> 8) & 0xFF; // High byte
+
+  can3.write(msg);
+  Serial.print("Sent 3000 as: ");
+  Serial.print(msg.buf[0], HEX);
+  Serial.print(" ");
+  Serial.println(msg.buf[1], HEX);
+
+  delay(1000);
 }
