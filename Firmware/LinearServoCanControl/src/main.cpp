61bf756aa17356d3b4f835620261c7dc5bacd283
Tony Shilati <anthonyshilati@gmail.com>
2025-10-31 14:59:25 -0500

    Added code to send commands to the linear servo and read the encoder data. Telemetry is not working well

--- Firmware/LinearServoCanControl/src/main.cpp
+++ Firmware/LinearServoCanControl/src/main.cpp
@@ -1,37 +1,41 @@
 #include <FlexCAN_T4.h>
 #include <math.h>
+#include <QuadEncoder.h>
 
-#define FREQUENCY_1 0.0f                  // Hz
-#define FREQUENCY_2 20.0f                 // Hz
-#define INITIAL_AMPLITUDE 1.93f           // mm
-#define INITIAL_AMPLITUDE_TICKS 2800.0f   // mm
-#define FINAL_AMPLITUDE 0.51f             // mm
-#define SWEEP_LENGTH 100.0f                // s
-#define SEND_INTERVAL 1                   // ms
+#define FREQUENCY 1.0f                  // Hz
+#define AMPLITUDE 1800                  // Ticks
+#define SWEEP_LENGTH 5.0f              // s
 
-static uint32_t lastMillis = 0;
 
-// Use CAN3 interface for the 3rd CAN bus on Teensy 4.1
-FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> can3;
 
-uint16_t number;
-unsigned long start;
-unsigned long start_micros;
-float tau_inv = -log(FINAL_AMPLITUDE/INITIAL_AMPLITUDE) / SWEEP_LENGTH; // Inverse time constant of exponential decay
+/*////////
+ * Object Instantiation
+ *////////
+FlexCAN_T4        <CAN3, RX_SIZE_256, TX_SIZE_16> can3;
+QuadEncoder       encoder1(3, 5, 7);  // ENC1 using pins 0 (A) and 1 (B)
 
-void printMessage(const CAN_message_t &m) {
-  // print telemetry data and timestamp
+IntervalTimer     servoTimer;
+IntervalTimer     encoderTimer;
 
-  if (m.len >= 2) {
-    uint16_t value = (uint16_t)m.buf[0] | ((uint16_t)m.buf[1] << 8); // low byte first
-    Serial.print(value);           // decimal
-    Serial.print(", ");
-    Serial.println((micros() - start_micros)/1000000.0f, 4);
-  } else {
-    Serial.println("  (not enough data for 16-bit)");
-  }
-}
 
+/*////////
+ * Global Vars
+ *////////
+uint16_t          number;
+unsigned long     start;
+unsigned long     start_micros;
+
+/*////////
+ * Function Prototypes
+ *////////
+void readEncoder();
+void printMessage(const CAN_message_t &m);
+void commandServo();
+
+
+/*////////
+ * Setup Code
+ *////////
 void setup() {
   Serial.begin(115200);
   while (!Serial) {
@@ -39,13 +43,43 @@ void setup() {
   }
   Serial.println("Ready to go");
   
+  /*////////
+   * Config the linear servo comms
+   *////////
   can3.begin();
   can3.setBaudRate(1000000); // 1 Mbps
+
+  /*////////
+   * Configure the encoder
+   *////////
+  encoder1.setInitConfig();   // load default settings
+  encoder1.init();            // initialize hardware encoder
+
+  // Put the Quadrature chip in RS-422 mode
+  pinMode(26, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+
+  digitalWrite(26, LOW);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, LOW);
   
+  // Start the timers
   start = millis();
   start_micros = micros();
+
+  /*////////
+   * Start the control and DAQ timers
+   *////////
+  encoderTimer.begin(readEncoder, 3125);  // 320 Hz timer
+  delayMicroseconds(300);
+  servoTimer.begin(commandServo, 1000); // Send position commands at 1kHz
 }
 
+
+/*////////
+ * Main Loop
+ *////////
 void loop() {
   if ((millis() - start) > SWEEP_LENGTH * 1000){
     delay(3000);
@@ -57,29 +91,51 @@ void loop() {
 
   // Receive any incoming CAN messages and print them to Serial
   CAN_message_t msg;
+  noInterrupts();
   if (can3.read(msg)) {
     printMessage(msg);
   }
+  interrupts();
+}
 
-  // Optional: example transmitter (enable by defining SEND_EXAMPLE)
-  
 
-  if (millis() - lastMillis >= (uint32_t)SEND_INTERVAL) {
-    lastMillis = millis();
+/*////////
+ * Function declarations
+ *////////
 
-    float t = millis() / 1000.0f; // seconds
-    const float center = 8212.0f;
-    float value = center + INITIAL_AMPLITUDE_TICKS * exp(-t*tau_inv) *
-      sinf(2.0f * PI * ((FREQUENCY_1 * t) + (FREQUENCY_2 - FREQUENCY_1) / (2.0f*SWEEP_LENGTH) * t*t));
-    number = (uint16_t)roundf(value);
+void printMessage(const CAN_message_t &m) {
+  // print telemetry data and timestamp
 
-    CAN_message_t tx;
-    tx.id = 0x3;
-    tx.len = 2; // Sending 2 bytes
-    tx.buf[0] = number & 0xFF;        // Low byte
-    tx.buf[1] = (number >> 8) & 0xFF; // High byte
-    can3.write(tx);
-
+  if (m.len >= 2) {
+    uint16_t value = (uint16_t)m.buf[0] | ((uint16_t)m.buf[1] << 8); // low byte first
+    Serial.print("T:");
+    Serial.print(value);           // decimal
+    Serial.print(",");
+    Serial.println((micros() - start_micros)/1000000.0f, 4);
+  } else {
+    Serial.println("  (not enough data for 16-bit)");
   }
+}
+
+void readEncoder(){
+  Serial.print("E:");
+  Serial.print(encoder1.read()*1e-3, 3);
+  Serial.print(", ");
+  Serial.println((micros() - start_micros)/1000000.0f, 4);
+}
+
+void commandServo(){
+
+  float t = millis() / 1000.0f; // seconds
+  const float center = 8212.0f;
+  float value = center + AMPLITUDE * sinf(2.0f * PI * FREQUENCY * t);
+  number = (uint16_t)roundf(value);
+
+  CAN_message_t tx;
+  tx.id = 0x3;
+  tx.len = 2; // Sending 2 bytes
+  tx.buf[0] = number & 0xFF;        // Low byte
+  tx.buf[1] = (number >> 8) & 0xFF; // High byte
+  can3.write(tx);
 
 }
