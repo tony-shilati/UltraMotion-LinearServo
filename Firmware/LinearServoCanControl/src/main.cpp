cf17fb73375c7418f93cdfad9572660603bf24b5
Tony Shilati <anthonyshilati@gmail.com>
2025-11-05 14:35:27 -0600

    Working with single sine wave: plots telemtry, encoder, ground/command

--- Firmware/LinearServoCanControl/src/main.cpp
+++ Firmware/LinearServoCanControl/src/main.cpp
@@ -3,7 +3,7 @@
 #include <QuadEncoder.h>
 
 #define FREQUENCY 10.0f                  // Hz
-#define AMPLITUDE 5000                  // Ticks
+#define AMPLITUDE 865                 // Ticks
 #define SWEEP_LENGTH 5.0f              // s
 #define CENTER 8212
 
@@ -25,6 +25,7 @@ IntervalTimer     encoderTimer;
 uint16_t          number;
 unsigned long     start;
 unsigned long     start_micros;
+bool started = false;
 
 /*////////
  * Function Prototypes
@@ -49,6 +50,13 @@ void setup() {
    *////////
   can3.begin();
   can3.setBaudRate(1000000); // 1 Mbps
+
+
+  /*////////
+   * Start the linear servo commands
+   *////////
+  servoTimer.begin(commandServo, 1000); // Send position commands at 1kHz
+  delay(1000);
   
 
   /*////////
@@ -69,13 +77,14 @@ void setup() {
   // Start the timers
   start = millis();
   start_micros = micros();
+  started = true;
 
   /*////////
    * Start the control and DAQ timers
    *////////
   encoderTimer.begin(readEncoder, 3125);  // 320 Hz timer
-  delayMicroseconds(300);
-  servoTimer.begin(commandServo, 1000); // Send position commands at 1kHz
+  
+  
 }
 
 
@@ -96,7 +105,9 @@ void loop() {
   // Receive any incoming CAN messages and print them to Serial
   CAN_message_t msg;
   if (can3.read(msg)) {
+    noInterrupts();
     printMessage(msg);
+    interrupts();
   }
 }
 
@@ -128,10 +139,19 @@ void readEncoder(){
 
 void commandServo(){
 
-  float t = millis() / 1000.0f; // seconds
-  const float center = (float)CENTER;
-  float value = center + AMPLITUDE * sinf(2.0f * PI * FREQUENCY * t);
-  number = (uint16_t)roundf(value);
+  if (!started){
+    number = (float)CENTER;
+  } else {
+
+    float t = (millis() - start) / 1000.0f; // seconds
+    float value = (float)CENTER + AMPLITUDE * sinf(2.0f * PI * FREQUENCY * t);
+    number = (uint16_t)roundf(value);
+
+    Serial.print("GND:");
+    Serial.print(number);
+    Serial.print(", ");
+    Serial.println((micros() - start_micros)/1000000.0f, 4);
+  }
 
   CAN_message_t tx;
   tx.id = 0x3;
@@ -139,5 +159,7 @@ void commandServo(){
   tx.buf[0] = number & 0xFF;        // Low byte
   tx.buf[1] = (number >> 8) & 0xFF; // High byte
   can3.write(tx);
+
+  
 
 }
